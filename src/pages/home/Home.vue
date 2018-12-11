516a870238f96e3c937f4fef4f4686c3c0f93a45
anke <245783065@qq.com>
2018-12-11 17:05:21 +0800

    test

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="home" @click="handleHref">Home</div>
+    <router-link to="/list">列表页</router-link>
+  </div>
+</template>
+
+<script  type='text/ecmascript-6'>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style scoped>
+.home {
+  font-size: 50px;
+}
+</style>
---
+++ src/pages/list/List.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>List</div>
+</template>
+
+<script  type='text/ecmascript-6'>
+export default {
+  name: 'List'
+}
+</script>
+
+<style scoped>
+
+</style>
