569e50a1dbbe49286c61a5b376335b6a74cebfc2
anke <245783065@qq.com>
2018-12-12 11:42:45 +0800

    swiperEnd

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,13 +1,51 @@
 <template>
-  <div></div>
+  <div class="wrapper">
+      <swiper :options="swiperOption" ref="mySwiper">
+    <!-- slides -->
+    <swiper-slide v-for="item in swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" alt="去哪儿玩">
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
 </template>
 
 <script  type='text/ecmascript-6'>
 export default {
-
+  name: 'homeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1812/a8/90d66a621daf6802.jpg_750x200_d479b789.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/f7/72ce947f4dff4c02.jpg_750x200_aa583dba.jpg'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1810/af/30ffcf34e9819f02.jpg_750x200_736b7236.jpg'
+      }]
+    }
+  }
 }
 </script>
 
 <style scoped lang='stylus' rel='stylesheet/stylus'>
-
+  .wrapper >>> .swiper-pagination-bullet-active
+      background: #fff
+  .wrapper
+    width: 100%
+    height: 0
+    overflow: hidden
+    padding-bottom: 26.7%
+    background: #eee
+    .swiper-img
+      width: 100%
 </style>
