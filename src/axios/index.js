16cacab6fcb5a39514c56a84e6f0539c5032b6b7
anke <245783065@qq.com>
2018-12-23 20:39:48 +0800

    axios

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-function indexList (url) {
+export default function indexList (url) {
   let baseUrl = ' https://www.easy-mock.com/mock/5c1f35b795d59f6b419e0015/travel/'
   return axios({
     url: url,
@@ -10,5 +10,3 @@ function indexList (url) {
     return Promise.resolve(res.data)
   })
 }
-
-export default indexList
