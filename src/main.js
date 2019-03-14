c5d780ee5163eef842e5666d7090c5ee5ab69e8b
anke <245783065@qq.com>
2019-03-14 12:22:31 +0800

    vedioEnd

--- src/main.js
+++ src/main.js
@@ -1,20 +1,26 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+const FastClick = require('static/vedio-fastclick.js')
+import 'videojs-contrib-hls'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import fastclick from 'fastclick'
 import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store'
 import VueLazyload from 'vue-lazyload'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
+import VueVideoPlayer from 'vue-video-player'
+// require videojs style
+import 'video.js/dist/video-js.css'
+import 'styles/video.css'
 // require styles
 import 'swiper/dist/css/swiper.css'
 
+Vue.use(VueVideoPlayer)
+FastClick.attach(document.body)
 Vue.config.productionTip = false
-fastclick.attach(document.body)
 Vue.use(VueAwesomeSwiper)
 Vue.prototype.bus = new Vue()
 Vue.use(VueLazyload, {
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -5,6 +5,15 @@
     <home-icons :list="iconList"></home-icons>
     <home-recommend :list="recommendList"></home-recommend>
     <home-weekend :list="weekendList"></home-weekend>
+    <div class="player">
+      <video-player class="video-player vjs-custom-skin vjs-big-play-centered"
+                    ref="videoPlayer"
+                    :playsinline="true"
+                    :options="playerOptions"
+                    @play="onPlayerPlay($event)"
+                    @pause="onPlayerPause($event)">
+      </video-player>
+    </div>
   </div>
 </template>
 
@@ -16,6 +25,7 @@ import HomeRecommend from './components/Recommend'
 import HomeWeekend from './components/Weekend'
 import Axios from '@/axios/index'
 import { mapState } from 'vuex'
+import { videoPlayer } from 'vue-video-player'
 export default {
   name: 'Home',
   components: {
@@ -23,7 +33,8 @@ export default {
     HomeSwiper,
     HomeIcons,
     HomeRecommend,
-    HomeWeekend
+    HomeWeekend,
+    videoPlayer
   },
   data () {
     return {
@@ -31,14 +42,75 @@ export default {
       swiperList: [],
       iconList: [],
       recommendList: [],
-      weekendList: []
+      weekendList: [],
+      // playerOptions: {
+      //   autoplay: false, //如果true,浏览器准备好时开始回放。
+      //   muted: false, // 默认情况下将会消除任何音频。
+      //   loop: false, // 导致视频一结束就重新开始。
+      //   preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+      //   language: 'zh-CN',
+      //   aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+      //   fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+      //   playbackRates: [0.7, 1.0, 1.5, 2.0],
+      //   sources: [{
+      //     type: "video/mp4",
+      //     src: "https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm"
+      //   }],
+      //   poster: "/static/images/author.jpg",
+      //   width: document.documentElement.clientWidth,
+      //   notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+      // }
+       playerOptions: {
+        playbackRates: [0.7, 1.0, 1.5, 2.0], //播放速度
+        autoplay: false, //如果true,浏览器准备好时开始回放。
+        muted: false, // 默认情况下将会消除任何音频。
+        loop: false, // 导致视频一结束就重新开始。
+        preload: 'auto', // 建议浏览器在<video>加载元素后是否应该开始下载视频数据。auto浏览器选择最佳行为,立即开始加载视频（如果浏览器支持）
+        language: 'zh-CN',
+        aspectRatio: '16:9', // 将播放器置于流畅模式，并在计算播放器的动态大小时使用该值。值应该代表一个比例 - 用冒号分隔的两个数字（例如"16:9"或"4:3"）
+        fluid: true, // 当true时，Video.js player将拥有流体大小。换句话说，它将按比例缩放以适应其容器。
+        sources: [{
+          type: "video/mp4",
+          src: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" //你的视频地址（必填）
+        }],
+        poster: "poster.jpg", //你的封面地址
+        width: document.documentElement.clientWidth,
+        notSupportedMessage: '此视频暂无法播放，请稍后再试', //允许覆盖Video.js无法播放媒体源时显示的默认信息。
+        controlBar: {
+          timeDivider: true,
+          durationDisplay: true,
+          remainingTimeDisplay: false,
+          fullscreenToggle: true  //全屏按钮
+        }
+      }
     }
   },
   mounted () {
     this.lastCity = this.city
     this.getHomeInfo()
+    console.log('this is current player instance object', this.player)
   },
   methods: {
+    // listen event
+    onPlayerPlay (player) {
+      // console.log('player play!', player)
+    },
+    onPlayerPause (player) {
+      // console.log('player pause!', player)
+    },
+    // ...player event
+
+    // or listen state event
+    playerStateChanged (playerCurrentState) {
+      // console.log('player current update state', playerCurrentState)
+    },
+
+    // player is ready
+    playerReadied (player) {
+      console.log('the player is readied', player)
+      // you can use it to do something...
+      // player.[methods]
+    },
     getHomeInfo () {
       Axios.ajax('/index?city=' + this.city).then((res) => {
         if (res.ret && res.data) {
@@ -60,11 +132,15 @@ export default {
   computed: {
     ...mapState([
       'city'
-    ])
+    ]),
+    player () {
+      return this.$refs.videoPlayer.player
+    }
   }
 }
 </script>
 
 <style scoped lang='stylus' rel='stylesheet/stylus'>
-
+.list
+  padding-bottom: 100px
 </style>
